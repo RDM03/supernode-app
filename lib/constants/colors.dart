5bda3103f34762d5a0e7d26584cb4d3dac7bebb5
Shargeel <shargeel@mxc.org>
2020-03-28 17:04:41 +0100

    project setup with basic boilerplate

---
+++ lib/constants/colors.dart
@@ -0,0 +1,16 @@
+/*
+*  colors.dart
+*  LPWAN app copy
+*
+*  Created by shargeel.
+*  Copyright © 2018 MXC. All rights reserved.
+    */
+
+import 'dart:ui';
+
+
+class AppColors {
+  static const Color color = Color.fromARGB(255, 28, 20, 120);
+  static const Color color2 = Color.fromARGB(255, 76, 137, 230);
+  static const Color color3 = Color.fromARGB(255, 227, 231, 233);
+}
