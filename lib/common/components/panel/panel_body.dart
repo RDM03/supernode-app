5f7d3af29dea3da527eaf37725a5b540ef2279ac
lei1996 <617218165@qq.com>
2020-05-30 18:21:48 +0800

    Adjust the layout of the gateway widget

--- lib/common/components/panel/panel_body.dart
+++ lib/common/components/panel/panel_body.dart
@@ -2,46 +2,65 @@ import 'package:flutter/material.dart';
 import 'package:supernodeapp/theme/colors.dart';
 import 'package:supernodeapp/theme/font.dart';
 
-Widget panelBody({String titleText,String subtitleText,String trailTitle, trailSubtitle,IconData icon,Function onPressed}){
-  return ListTile(
-    contentPadding: EdgeInsets.zero,
-    leading: Container(
-      margin: const EdgeInsets.only(left: 10),
-      child: IconButton(
-        padding: EdgeInsets.zero,
-        icon: Icon(
-          icon,
-          color: buttonPrimaryColor,
-          size: 50,
+Widget panelBody(
+    {String titleText,
+    String subtitleText,
+    String trailTitle,
+    trailSubtitle,
+    IconData icon,
+    Function onPressed}) {
+  var temp = trailSubtitle.split('(');
+  String mxcPrice = temp[0].substring(0, temp[0].length - 1);
+  String usdPrice = temp[1].substring(0, temp[1].length - 1);
+
+  return Container(
+    padding: EdgeInsets.all(10),
+    child: Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: <Widget>[
+        Row(
+          children: <Widget>[
+            Container(
+                child: IconButton(
+              padding: EdgeInsets.zero,
+              icon: Icon(
+                icon,
+                color: buttonPrimaryColor,
+                size: 50,
+              ),
+              onPressed: onPressed,
+            )),
+            SizedBox(width: 12),
+            Text(
+              titleText,
+              textAlign: TextAlign.left,
+              style: kMiddleFontOfBlack,
+            ),
+            SizedBox(width: 5),
+            Text(
+              subtitleText,
+              textAlign: TextAlign.left,
+              style: kBigFontOfBlack,
+            ),
+          ],
         ),
-        onPressed: onPressed,
-      )
-    ),
-    title: Text(
-      titleText,
-      textAlign: TextAlign.left,
-      style: kMiddleFontOfBlack,
-    ),
-    subtitle: Text(
-      subtitleText,
-      textAlign: TextAlign.left,
-      style: kBigFontOfBlack,
-    ),
-    trailing: Container(
-      margin: EdgeInsets.only(top: 10, right: 16),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          Text(
-            trailTitle,
-            style: kSmallFontOfGrey
+        Container(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              Text(trailTitle, style: kSmallFontOfGrey),
+              Text(
+                mxcPrice,
+                style: kMiddleFontOfBlack,
+              ),
+              Text(
+                usdPrice,
+                style: kMiddleFontOfBlack,
+              )
+            ],
           ),
-          Text(
-            trailSubtitle,
-            style: kMiddleFontOfBlack,
-          )
-        ]
-      )
-    )
+        ),
+      ],
+    ),
   );
 }
--- lib/common/components/summary_row.dart
+++ lib/common/components/summary_row.dart
@@ -4,45 +4,72 @@ import 'package:supernodeapp/theme/colors.dart';
 import 'package:supernodeapp/theme/font.dart';
 import 'package:supernodeapp/theme/spacing.dart';
 
-Widget summaryRow({String image = '',String title = '',String subtitle = '',String number ='',String price = ''}){
-  return ListTile(
-    leading: Stack(
-      alignment: Alignment.center,
-      children: [
-        Image.asset(
-          AppImages.blueCircle,
-          fit: BoxFit.none,
-          color: lightBlue,
-        ),
-        Image.asset(
-          image,
-          fit: BoxFit.none,
-        )
-      ]
-    ),
-    title: Text(
-      title,
-      style: kMiddleFontOfBlack,
-    ),
-    subtitle: Text(
-      number,
-      style: kBigFontOfBlue,
-    ),
-    trailing: Container(
-      margin: kOuterRowTop10,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
+Widget summaryRow(
+    {String image = '',
+    String title = '',
+    String subtitle = '',
+    String number = '',
+    String price = ''}) {
+  var temp = price.split('(');
+  String mxcPrice = temp[0].substring(0, temp[0].length - 1);
+  print(mxcPrice);
+  String usdPrice = temp[1].substring(0, temp[1].length - 1);
+
+  return Container(
+      padding: EdgeInsets.all(10),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: <Widget>[
-          Text(
-            subtitle,
-            style: kSmallFontOfGrey,
+          Row(
+            children: <Widget>[
+              Stack(alignment: Alignment.center, children: [
+                Image.asset(
+                  AppImages.blueCircle,
+                  fit: BoxFit.none,
+                  color: lightBlue,
+                ),
+                Image.asset(
+                  image,
+                  fit: BoxFit.none,
+                )
+              ]),
+              SizedBox(width: 12),
+              Text(
+                title,
+                style: kMiddleFontOfBlack,
+              ),
+              SizedBox(width: 5),
+              Text(
+                number,
+                style: kBigFontOfBlue,
+              ),
+            ],
           ),
-          Text(
-            price,
-            style: kMiddleFontOfBlack,
+          Container(
+            margin: kOuterRowTop10,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: <Widget>[
+                Text(
+                  subtitle,
+                  style: kSmallFontOfGrey,
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: <Widget>[
+                    Text(
+                      mxcPrice,
+                      style: kMiddleFontOfBlack,
+                    ),
+                    Text(
+                      usdPrice,
+                      style: kMiddleFontOfBlack,
+                    ),
+                  ],
+                )
+              ],
+            ),
           )
         ],
-      ),
-    )
-  );
+      ));
 }
