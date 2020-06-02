5a068f197ce25ed99b3ecd22498e3741eb1b970f
NamGyeong <namgyeong.cho@mxc.org>
2020-06-02 14:09:38 +0200

    MMA-56 added non otp secirty code input ui

---
+++ lib/page/set_2fa_page/enter_nonopt_securitycode_component/view.dart
@@ -0,0 +1,68 @@
+import 'package:fish_redux/fish_redux.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_i18n/flutter_i18n.dart';
+import 'package:supernodeapp/common/components/buttons/primary_button.dart';
+import 'package:supernodeapp/common/components/page/paragraph.dart';
+import 'package:supernodeapp/common/components/text_field/text_field_with_title.dart';
+import 'package:supernodeapp/theme/colors.dart';
+import 'package:supernodeapp/theme/spacing.dart';
+
+import '../action.dart';
+import 'action.dart';
+import 'state.dart';
+
+Widget buildView(EnterNonOTPSecurityCodeState state, Dispatch dispatch, ViewService viewService) {
+  var _ctx = viewService.context;
+print('enter security');
+  print(state.isEnabled);
+  return Scaffold(
+      resizeToAvoidBottomInset: false,
+      backgroundColor: cardBackgroundColor,
+      appBar: AppBar(
+        iconTheme: IconThemeData(color: Colors.black),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+      ),
+      body: SafeArea(
+          child: Container(
+              padding: kRoundRow202,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+                  //paragraph(FlutterI18n.translate(_ctx, 'send_email')),
+                  paragraph(FlutterI18n.translate(_ctx,'enter_security_01')),
+                  Form(
+                    key: state.formKey,
+                    autovalidate: false,
+                    child: Column(
+                        children: <Widget>[
+                          Container(
+                            margin: const EdgeInsets.only(top: 40),
+                            child: TextFieldWithTitle(
+                              title: FlutterI18n.translate(_ctx, 'withdraw_amount'),
+                              textInputAction: TextInputAction.next,
+                              //validator: (value) => _onValidAmount(_ctx,value),
+                              controller: state.secretCtl,
+                            ),
+                          ),
+                        ]
+                    ),
+                  ),
+                  Spacer(),
+                  state.isEnabled ?
+                  PrimaryButton(
+                      onTap: () => dispatch(Set2FAActionCreator.onSetDisable()),
+                      buttonTitle: FlutterI18n.translate(_ctx, 'confirm'),
+                      minHeight: 46
+                  ):
+                  PrimaryButton(
+                      onTap: () => dispatch(Set2FAActionCreator.onSetEnable()),
+                      buttonTitle: FlutterI18n.translate(_ctx, 'confirm'),
+                      minHeight: 46
+                  ),
+                ],
+              )
+          )
+      )
+  );
+}
