dbf8b0bb4dc4f75f3332b94845e13f5b80b6a26a
NamGyeong <namgyeong.cho@mxc.org>
2020-06-04 15:47:15 +0200

    MMA-71 ui changed

--- lib/page/withdraw_page/enter_securitycode_withdraw_component/view.dart
+++ lib/page/withdraw_page/enter_securitycode_withdraw_component/view.dart
@@ -1,11 +1,13 @@
+
 import 'package:fish_redux/fish_redux.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_i18n/flutter_i18n.dart';
-import 'package:supernodeapp/common/components/buttons/primary_button.dart';
-import 'package:supernodeapp/common/components/page/paragraph.dart';
+import 'package:supernodeapp/common/components/page/page_frame.dart';
+import 'package:supernodeapp/common/components/page/page_nav_bar.dart';
 import 'package:supernodeapp/common/components/text_field/text_field_with_codes.dart';
-import 'package:supernodeapp/theme/colors.dart';
-import 'package:supernodeapp/theme/spacing.dart';
+import 'package:supernodeapp/common/components/page/submit_button.dart';
+import 'package:supernodeapp/common/components/buttons/primary_button.dart';
+import 'package:supernodeapp/common/utils/reg.dart';
 
 import '../action.dart';
 import 'action.dart';
@@ -13,114 +15,117 @@ import 'state.dart';
 
 Widget buildView(EnterSecurityCodeWithdrawState state, Dispatch dispatch, ViewService viewService) {
   var _ctx = viewService.context;
+  state.isEnabled = true;
+  return pageFrame(
+      context: viewService.context,
+      children: [
+        pageNavBar(
+            FlutterI18n.translate(_ctx, 'withdraw'),
+            onTap: () => Navigator.pop(viewService.context)
+        ),
+        Container(
+          padding: EdgeInsets.fromLTRB(0.0, 20.0, 0.0, 0.0),
+          alignment: FractionalOffset(0.5, 0.5),
+          child: Column(
 
-  return Scaffold(
-      resizeToAvoidBottomInset: false,
-      backgroundColor: cardBackgroundColor,
-      appBar: AppBar(
-        iconTheme: IconThemeData(color: Colors.black),
-        title: Text(
-            FlutterI18n.translate(_ctx,'withdraw'),
+              children: <Widget>[
+                Text(
+                    FlutterI18n.translate(_ctx,'wthdr_ent_code_01'),
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.w400,
+                      fontSize: 24,
+                    )
+                ),
+              ]
+          ),
+        ),
+        Container(
+          padding: EdgeInsets.symmetric(vertical: 5.0),
+          alignment: FractionalOffset(0.5, 0.5),
+          child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                    FlutterI18n.translate(_ctx,'wthdr_ent_code_02'),
+                    style: TextStyle(
+                      color: Colors.grey[600],
+                      fontWeight: FontWeight.w400,
+                    )
+                ),
+              ]
+          ),
+        ),
+        Container(
+          padding: EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 80.0),
+          alignment: FractionalOffset(0.5, 0.5),
+          child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                    FlutterI18n.translate(_ctx,'wthdr_ent_code_03'),
+                    style: TextStyle(
+                      color: Colors.grey[600],
+                      fontWeight: FontWeight.w400,
+                    )
+                ),
+              ]
+          ),
+        ),
+        Text(
+            FlutterI18n.translate(_ctx,'wthdr_ent_code_04'),
+            textAlign: TextAlign.left,
             style: TextStyle(
-                color: Colors.black,
-                fontWeight: FontWeight.w400,
-                fontSize: 18,
+              color: Colors.grey[600],
+              fontWeight: FontWeight.w400,
+              fontSize: 12,
             )
-      ),
-        centerTitle: true,
-        backgroundColor: Colors.transparent,
-        elevation: 0,
-      ),
-      body: SafeArea(
-          child: Container(
-              padding: kRoundRow202,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: <Widget>[
-                  Container(
-                    padding: EdgeInsets.fromLTRB(0.0, 20.0, 0.0, 0.0),
-                    child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: <Widget>[
-                          Text(
-                              FlutterI18n.translate(_ctx,'wthdr_ent_code_01'),
-                              style: TextStyle(
-                                color: Colors.black,
-                                fontWeight: FontWeight.w400,
-                                fontSize: 24,
-                              )
-                          ),
-                        ]
-                    ),
-                  ),
-                  Container(
-                    padding: EdgeInsets.symmetric(vertical: 5.0),
-                    child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: <Widget>[
-                          Text(
-                              FlutterI18n.translate(_ctx,'wthdr_ent_code_02'),
-                              style: TextStyle(
-                                color: Colors.grey[600],
-                                fontWeight: FontWeight.w400,
-                              )
-                          ),
-                        ]
-                    ),
-                  ),
-                  Container(
-                    padding: EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 80.0),
-                    child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: <Widget>[
-                          Text(
-                              FlutterI18n.translate(_ctx,'wthdr_ent_code_03'),
-                              style: TextStyle(
-                                color: Colors.grey[600],
-                                fontWeight: FontWeight.w400,
-                              )
-                          ),
-                        ]
-                    ),
-                  ),
-                  Form(
-                    key: state.formKey,
-                    autovalidate: false,
-                    child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Text(
-                              FlutterI18n.translate(_ctx,'wthdr_ent_code_04'),
-                              textAlign: TextAlign.left,
-                              style: TextStyle(
-                                color: Colors.grey[600],
-                                fontWeight: FontWeight.w400,
-                                fontSize: 12,
-                              )
-                          ),
-                          Container(
-                            //margin: kOuterRowTop35,
-                            child: Row(
-                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                children: state.listCtls.asMap().keys.map((index) => textfieldWithCodes(
-                                    context: _ctx,
-                                    controller: state.listCtls[index],
-                                    isLast: index == state.listCtls.length - 1
-                                )).toList()
-                            ),
-                          ),
-                        ]
-                    ),
-                  ),
-                  Spacer(),
-                  PrimaryButton(
-                      onTap: () => dispatch(WithdrawActionCreator.onSubmit()),
-                      buttonTitle: FlutterI18n.translate(_ctx, 'confirm'),
-                      minHeight: 46
-                  ),
-                ],
-              )
-          )
-      )
+        ),
+        Container(
+          //margin: kOuterRowTop35,
+          child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: state.listCtls.asMap().keys.map((index) => textfieldWithCodes(
+                  context: _ctx,
+                  controller: state.listCtls[index],
+                  isLast: index == state.listCtls.length - 1
+              )).toList(),
+
+          ),
+        ),
+        Container(
+          padding: EdgeInsets.fromLTRB(0.0, 280.0, 0.0, 00.0),
+          child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                PrimaryButton(
+                    onTap: () => dispatch(WithdrawActionCreator.onSubmit()),
+                    buttonTitle: FlutterI18n.translate(_ctx, 'confirm'),
+                    minHeight: 46
+                ),
+              ]
+          ),
+        ),
+      ]
   );
+}
+
+String _onValidAmount(BuildContext context,String value){
+  String res = Reg.isEmpty(value);
+  if(res != null) return FlutterI18n.translate(context, res);
+
+  RegExp emailRule = new RegExp(r'^[1-9]\d*$');
+
+  if(!emailRule.hasMatch(value.trim())){
+    return FlutterI18n.translate(context, 'reg_amount');
+  }
+
+  return null;
+}
+
+String _onValidAddress(BuildContext context,String value){
+  String res = Reg.isEmpty(value);
+  if(res != null) return FlutterI18n.translate(context, res);
+
+  return null;
 }
