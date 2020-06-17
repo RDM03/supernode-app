1e5da24a497c060d80d20ae8292f7a8ac835c84d
Christian Gröschel <christian@mxc.org>
2020-06-17 16:34:18 +0200

    pinning cocoa pod install

--- ios/appcenter-post-clone.sh
+++ ios/appcenter-post-clone.sh
@@ -10,6 +10,14 @@ cd ..
 git clone -b beta https://github.com/flutter/flutter.git
 export PATH=`pwd`/flutter/bin:$PATH
 
+echo "Uninstalling all CocoaPods versions"
+sudo gem uninstall cocoapods --all --executables
+
+COCOAPODS_VER=`sed -n -e 's/^COCOAPODS: \([0-9.]*\)/\1/p' Podfile.lock`
+
+echo "Installing CocoaPods version $COCOAPODS_VER"
+sudo gem install cocoapods -v $COCOAPODS_VER
+
 flutter channel stable
 flutter doctor
 
