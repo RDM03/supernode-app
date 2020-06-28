0d5415ed2a6edc74597146480f11bcbd5ce3179e
Eric huang <1659897298@qq.com>
2020-06-29 00:49:02 +0800

    app version

--- lib/page/settings_page/about_component/effect.dart
+++ lib/page/settings_page/about_component/effect.dart
@@ -2,6 +2,7 @@ import 'package:fish_redux/fish_redux.dart';
 import 'package:flutter_i18n/flutter_i18n.dart';
 import 'package:supernodeapp/common/components/update_dialog.dart';
 import 'package:toast/toast.dart';
+import 'package:package_info/package_info.dart';
 
 import 'action.dart';
 import 'state.dart';
@@ -9,6 +10,7 @@ import 'state.dart';
 Effect<AboutState> buildEffect() {
   return combineEffects(<Object, Effect<AboutState>>{
     AboutAction.onCheckForUpdate: _onCheckForUpdate,
+    Lifecycle.initState: _init,
   });
 }
 
@@ -20,4 +22,14 @@ void _onCheckForUpdate(Action action, Context<AboutState> ctx) {
       Toast.show(FlutterI18n.translate(_ctx,'tip_latest_version'),_ctx,duration: 5);
     }
   });
+}
+void _init(Action action, Context<AboutState> ctx) {
+  PackageInfo.fromPlatform()
+      .then((info){
+        println(info.version);
+        println(info.appName);
+        println(info.packageName);
+        ctx.dispatch(AboutActionCreator.onInitPackageInfo(info));
+      }
+  );
 }
