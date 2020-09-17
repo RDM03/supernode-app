18b3a8d23353ce228aa9a15e136192ebfb40fd55
Ilya Beregovskiy <festeloqq@gmail.com>
2020-09-17 09:14:08 +0200

    MMA-230 language switch fix

--- lib/page/settings_page/language_component/action.dart
+++ lib/page/settings_page/language_component/action.dart
@@ -3,11 +3,11 @@ import 'package:fish_redux/fish_redux.dart';
 enum LanguageAction { select, onChange }
 
 class LanguageActionCreator {
-  static Action change(String language) {
-    return Action(LanguageAction.select,payload: language);
+  static Action select(String language) {
+    return Action(LanguageAction.select, payload: language);
   }
 
   static Action onChange(String language) {
-    return Action(LanguageAction.onChange,payload: language);
+    return Action(LanguageAction.onChange, payload: language);
   }
 }
