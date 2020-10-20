9c0c38baf6bceaa170a846d3c6ced11b809c84b8
Ilya Beregovskiy <festeloqq@gmail.com>
2020-10-20 09:59:31 +0300

    update local_auth

--- packages/local_auth/example/ios/Runner/GeneratedPluginRegistrant.m
+++ packages/local_auth/example/ios/Runner/GeneratedPluginRegistrant.m
@@ -4,12 +4,6 @@
 
 #import "GeneratedPluginRegistrant.h"
 
-#if __has_include(<e2e/E2EPlugin.h>)
-#import <e2e/E2EPlugin.h>
-#else
-@import e2e;
-#endif
-
 #if __has_include(<local_auth/FLTLocalAuthPlugin.h>)
 #import <local_auth/FLTLocalAuthPlugin.h>
 #else
@@ -19,7 +13,6 @@
 @implementation GeneratedPluginRegistrant
 
 + (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
-  [E2EPlugin registerWithRegistrar:[registry registrarForPlugin:@"E2EPlugin"]];
   [FLTLocalAuthPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTLocalAuthPlugin"]];
 }
 
