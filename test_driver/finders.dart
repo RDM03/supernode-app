e96272206a9aa0d0af8426033a81fe23012c0e58
stahldom <jeff@stahlnecker.me>
2020-11-03 17:34:24 +0100

    refactor to use finders map

--- test_driver/finders.dart
+++ test_driver/finders.dart
@@ -1,5 +1,7 @@
 import 'package:flutter_driver/flutter_driver.dart';
 
+// Naming Convention 'page' + 'DescribeItem'
+
 Map f = {
   'logoFinder': find.byValueKey('homeLogo'),
   'loginFinder': find.byValueKey('homeLogin'),
@@ -12,5 +14,16 @@ Map f = {
   'questionCircle': find.byValueKey('questionCircle'),
   'totalGatewaysDashboard': find.byValueKey('totalGatewaysDashboard'),
   'helpTextFinder': find.byValueKey('helpText'),
-  'infoDialog': find.byValueKey('infoDialog')
+  'infoDialog': find.byValueKey('infoDialog'),
+  'stakeFlex': find.byValueKey('stakeFlex'),
+  'stakeAmount': find.byValueKey('stakeAmount'),
+  'submitButton': find.byValueKey('submitButton'),
+  'backButtonFinder': find.byValueKey('backButton'),
+  'successIconFinder': find.byValueKey('successIcon'),
+  'stakeAmountView': find.byValueKey('stakeAmountView'),
+  'stakeButtonDashboard': find.byValueKey('stakeButtonDashboard'),
+  'depositButtonDashboard': find.byValueKey('depositButtonDashboard'),
+  'exitPage': find.byValueKey('navActionButton'),
+  'settingsButtonDashboard': find.byValueKey('settingsButton'),
+  'logoutFinder': find.byValueKey('logout')
 };
--- test_driver/help_bubble_test.dart
+++ test_driver/help_bubble_test.dart
@@ -1,16 +1,12 @@
 import 'package:flutter_driver/flutter_driver.dart';
 import 'package:test/test.dart';
 
-import 'utils.dart' show delay, isPresent;
+import 'finders.dart' show f;
+import 'utils.dart' show isPresent;
 
 helpBubbleTest() {
   FlutterDriver driver;
   group('help bubble tests', () {
-    final infoDialog = find.byValueKey('infoDialog');
-    final stakeButtonDashboard = find.byValueKey('stakeButtonDashboard');
-    final questionCircle = find.byValueKey('questionCircle');
-    final backButtonFinder = find.byValueKey('backButton');
-
     setUpAll(() async {
       driver = await FlutterDriver.connect();
     });
@@ -21,13 +17,13 @@ helpBubbleTest() {
       }
     });
     test('can use MXC Vault help bubble', () async {
-      await driver.tap(stakeButtonDashboard);
+      await driver.tap(f['stakeButtonDashboard']);
       print('TAPPED STAKE BUTTON');
-      await driver.tap(questionCircle);
+      await driver.tap(f['questionCircle']);
       var isExists = await isPresent(find.byValueKey('helpText'), driver);
       expect(isExists, true);
-      await driver.tap(infoDialog);
-      await driver.tap(backButtonFinder);
+      await driver.tap(f['infoDialog']);
+      await driver.tap(f['backButtonFinder']);
     });
   });
 }
