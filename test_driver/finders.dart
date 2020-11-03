a6bed3e3043385079f7db157345b6b0dcdf1a558
stahldom <jeff@stahlnecker.me>
2020-11-03 17:13:28 +0100

    add finders map

---
+++ test_driver/finders.dart
@@ -0,0 +1,16 @@
+import 'package:flutter_driver/flutter_driver.dart';
+
+Map f = {
+  'logoFinder': find.byValueKey('homeLogo'),
+  'loginFinder': find.byValueKey('homeLogin'),
+  'menuFinder': find.byValueKey('homeSupernodeMenu'),
+  'emailFieldFinder': find.byValueKey('homeEmail'),
+  'passwordFieldFinder': find.byValueKey('homePassword'),
+  'testServerFinder': find.byValueKey('MXCbuild'),
+  'scrollMenu': find.byValueKey('scrollMenu'),
+  'mxcChinaFinder': find.byValueKey('MXCChina'),
+  'questionCircle': find.byValueKey('questionCircle'),
+  'totalGatewaysDashboard': find.byValueKey('totalGatewaysDashboard'),
+  'helpTextFinder': find.byValueKey('helpText'),
+  'infoDialog': find.byValueKey('infoDialog')
+};
