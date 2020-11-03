31e4d47c0f598793fa8064fd1efead9463e3b61a
stahldom <jeff@stahlnecker.me>
2020-11-03 15:44:11 +0100

    move login test to it's own file

--- test_driver/app_test.dart
+++ test_driver/app_test.dart
@@ -2,29 +2,8 @@ import 'package:dotenv/dotenv.dart' show load, env;
 import 'package:flutter_driver/flutter_driver.dart';
 import 'package:test/test.dart';
 
-Future<void> delay([int milliseconds = 250]) async {
-  await Future<void>.delayed(Duration(milliseconds: milliseconds));
-}
-
-isPresent(SerializableFinder byValueKey, FlutterDriver driver,
-    {Duration timeout = const Duration(seconds: 20)}) async {
-  try {
-    await driver.waitFor(byValueKey, timeout: timeout);
-    return true;
-  } catch (exception) {
-    return false;
-  }
-}
-
-canTap(SerializableFinder byValueKey, FlutterDriver driver,
-    {Duration timeout = const Duration(seconds: 2)}) async {
-  try {
-    await driver.tap(byValueKey, timeout: timeout);
-    return true;
-  } catch (exception) {
-    return false;
-  }
-}
+import 'login_test.dart' show loginPage;
+import 'utils.dart' show delay, canTap, isPresent;
 
 void main() {
   load();
@@ -96,59 +75,7 @@ void main() {
       }
     });
 
-    test('login help bubble works', () async {
-      await driver.waitUntilFirstFrameRasterized();
-
-      print('LOCATING THE MXC LOGO');
-
-      await driver.waitFor(logoFinder);
-
-      print('CLICK QUESTION CIRCLE');
-      await driver.waitFor(questionCircle);
-      await driver.tap(questionCircle);
-      // find solution for testing all languages
-      await driver.waitFor(helpTextFinder);
-      final helpTextExists = await isPresent(helpTextFinder, driver);
-      expect(helpTextExists, true);
-      await driver.tap(infoDialog);
-    });
-
-    test('can login', () async {
-      await driver.waitUntilFirstFrameRasterized();
-      print('LOCATING THE MXC LOGO');
-      await driver.waitFor(logoFinder);
-      print('LOADED, BEGINNING THE TAP');
-      for (var i = 0; i < 7; i++) {
-        await driver.tap(logoFinder);
-        delay(20);
-        print('TAP ${i + 1}');
-      }
-      print('ALL TAPPED OUT, LETS SELECT THAT SERVER');
-      await driver.tap(menuFinder);
-      await delay(2000);
-      await driver.scrollUntilVisible(scrollMenu, mxcChinaFinder);
-      var openMenuState = await canTap(testServerFinder, driver);
-      print(openMenuState);
-      if (openMenuState == true) {
-        print('TEST SERVER SELECTED');
-      } else {
-        print("OOPS THE MENU CLOSED, I'LL JUST OPEN THAT UP FOR YOU");
-        await driver.tap(menuFinder);
-        await driver.tap(testServerFinder);
-        print('SERVER SELECTED, TIME TO ENTER CREDENTIALS');
-      }
-      await driver.waitFor(emailFieldFinder);
-      print('I SEE THE EMAIL FIELD');
-      await driver.tap(emailFieldFinder);
-      await driver.enterText(env['TESTING_USER']);
-      await driver.waitFor(find.text(env['TESTING_USER']));
-      await driver.tap(passwordFieldFinder);
-      await driver.enterText(env['TESTING_PASSWORD']);
-      print('THE MOMENT HAS COME, WILL IT WORK?');
-      await driver.tap(loginFinder);
-      expect(await driver.getText(totalGatewaysDashboard), 'Revenue');
-      print('HOUSTON, WE ARE LOGGED IN');
-    }, timeout: Timeout(Duration(seconds: 60)));
+    loginPage();
 
     // test('has top up address', () async {
     //
