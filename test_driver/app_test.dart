4bfb5bd5411873b0aefa1e6059d58b176e6e3081
sbcodes <#=%)PG*^o+=ReBdt,5+>
2020-11-03 15:42:04 +0100

    fixed merge

--- test_driver/app_test.dart
+++ test_driver/app_test.dart
@@ -63,8 +63,10 @@ void main() {
 
     final stakeFlex = find.byValueKey('stakeFlex');
     final stakeAmount = find.byValueKey('stakeAmount');
-    final stakeButton = find.byValueKey('stakeButton');
-    final submitButtonTimeout = find.byValueKey('submitButtonTimeout');
+    final submitButton = find.byValueKey('submitButton');
+    final backButtonFinder = find.byValueKey('backButton');
+    final successIconFinder = find.byValueKey('successIcon');
+    final stakeAmountView = find.byValueKey('stakeAmountView');
 
     // Miner Page
 
@@ -196,35 +198,43 @@ void main() {
     //complete withdraw test
 
 
-// Staking Test doesn't Work
-//     test('can set stake', () async {
-//       /*
-//       Not clickable yet
-//       print('CHECKING ? BUTTON');
-//       await driver.waitFor(questionCircle);
-//       await driver.tap(questionCircle);
-//       delay(5000);
-//       var isExists = await isPresent(find.byValueKey('helpText'), driver);
-//       expect(isExists, true);
-//       delay(5000);
-//       await driver.waitFor(logoFinder);
-//       //not sure how to click something that isn't labelled so the you must close the help box manually for now
-//        */
-//
-//       await driver.tap(stakeButtonDashboard);
-//       await driver.tap(stakeFlex);
-//       await driver.tap(stakeAmount);
-//       await driver.enterText('20');
-//       await driver.waitFor(find.text('20'));
-//       await driver.tap(stakeButton);
-//       await driver.waitFor(find.text('(0)'));
-//       await delay(200);
-//       await driver.waitFor(submitButtonTimeout);
-//       await driver.tap(submitButtonTimeout);
-//       await driver.tap(exitPage);
-//
-//
-//     }, timeout:Timeout(Duration(seconds: 60)));
+    test('can set stake', () async {
+      await driver.waitUntilFirstFrameRasterized();
+      await driver.tap(stakeButtonDashboard);
+      print('tapped stake button');
+      await driver.waitUntilNoTransientCallbacks();
+      await driver.tap(stakeFlex);
+      for (var pageChanged = await isPresent(stakeAmount, driver);
+      pageChanged == false;
+      pageChanged = await isPresent(stakeAmount, driver)) {
+        print('page not changed, tapping again');
+        await driver.tap(stakeFlex);
+      }
+      print('tapped stakeFlex');
+      await driver.scrollUntilVisible(stakeAmountView, stakeAmount);
+      print('scrolled it visible');
+      await driver.tap(stakeAmount);
+      print('tapped stake amount');
+      await driver.enterText('20');
+      print('entered 20');
+      await driver.waitFor(find.text('20'));
+      print('20 is there');
+      await driver.tap(submitButton);
+      print('tapped stake button');
+      await delay(31000);
+      await driver.tap(submitButton);
+      print('Waited for the countdown, and proceeded anyways');
+      expect(await isPresent(successIconFinder, driver), true);
+      await driver.tap(exitPage);
+      print('tapped exit confirm');
+      await driver.scroll(stakeAmountView, 0, 300, Duration(milliseconds: 500));
+      await driver.tap(exitPage);
+      print('tapped exit stake amount page');
+      await driver.tap(backButtonFinder);
+      print(
+          'current page is ${await isPresent(depositButtonDashboard, driver) ? "Home" : "Not home, we're lost"}');
+    }, timeout: Timeout(Duration(seconds: 600)));
+
 
     test('add miner', () async {
       driver.tap(bottomNavBar_Gateway);
