861a2ff54e6e2331f4d2ed068252b094c17b3ac6
stahldom <jeff@stahlnecker.me>
2020-11-05 07:47:26 +0100

    add staking success icon key

--- lib/common/components/page/done.dart
+++ lib/common/components/page/done.dart
@@ -1,19 +1,19 @@
 import 'package:flutter/material.dart';
 
-Widget done({bool success = true}){
+Widget done({bool success = true}) {
   return Center(
     child: Container(
-      width: 80,
-      height: 80,
-      margin: const EdgeInsets.only(top: 50),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.all(Radius.circular(4)),
-      ),
-      child: Icon(
-        success ? Icons.check_circle_outline : Icons.info_outline,
-        color: success ? Colors.green : Colors.blue,
-        size: 80,
-      )
-    ),
+        width: 80,
+        height: 80,
+        margin: const EdgeInsets.only(top: 50),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.all(Radius.circular(4)),
+        ),
+        child: Icon(
+          success ? Icons.check_circle_outline : Icons.info_outline,
+          color: success ? Colors.green : Colors.blue,
+          size: 80,
+          key: Key('successIcon_$success'),
+        )),
   );
 }
