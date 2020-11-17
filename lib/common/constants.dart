25ef7192fd8514e7121eb070126d981214482ac8
miljan-mxc <72385784+miljan-mxc@users.noreply.github.com>
2020-11-17 12:09:41 +0100

    WeChat console project for debug Android app (signature)

--- lib/common/constants.dart
+++ lib/common/constants.dart
@@ -1,3 +1,4 @@
 library constants;
 
-const String WECHAT_APP_ID = "wx496db1d33c8fda79";
+const String WECHAT_APP_ID = "wx496db1d33c8fda79";//production
+//const String WECHAT_APP_ID = "wx7fcea6540bed2f37";//debug
