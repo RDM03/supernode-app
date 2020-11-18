4fd67c92ebdaaf90ba49a0aaf003e13f97f4151a
sbcodes <#=%)PG*^o+=ReBdt,5+>
2020-11-18 14:43:52 +0100

    homepage test complete

--- test_driver/app_test.dart
+++ test_driver/app_test.dart
@@ -34,15 +34,15 @@ void main() {
     if (env['ENVIRONMENT'] == 'test') {
       loginPageTests('MXCtest', env['DRIVE_MXCTEST_PASSWORD']);
       homeAPITest();
-      // stakingTest(env['DRIVE_MXCTEST_OTP_KEY']);
-      // helpBubbleTest();
-      // addMinerTest();
-      // deleteMinerTest();
+      stakingTest(env['DRIVE_MXCTEST_OTP_KEY']);
+      helpBubbleTest();
+      addMinerTest();
+      deleteMinerTest();
       logoutTest('MXCtest');
     } else
       for (var i = 0; i < s.length; i++) {
-
         loginPageTests(s[i][0], s[i][1]);
+        homeAPITest();
         stakingTest(s[i][2]);
         helpBubbleTest();
         //Currently miner tests don't work for servers other than test. Need miner serials.
