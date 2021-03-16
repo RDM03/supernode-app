badfc998112147f7cdf748a9e4f84d039385c76b
miljan-mxc <72385784+miljan-mxc@users.noreply.github.com>
2021-03-16 20:33:03 +0100

    Refactor ConfirmPage

--- lib/page/confirm_page/view.dart
+++ lib/page/confirm_page/view.dart
@@ -1,11 +1,12 @@
 import 'package:fish_redux/fish_redux.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_i18n/flutter_i18n.dart';
+import 'package:supernodeapp/common/components/buttons/primary_button.dart';
+import 'package:supernodeapp/common/components/column_spacer.dart';
 import 'package:supernodeapp/common/components/page/done.dart';
 import 'package:supernodeapp/common/components/page/page_content.dart';
 import 'package:supernodeapp/common/components/page/page_frame.dart';
 import 'package:supernodeapp/common/components/page/page_nav_bar.dart';
-import 'package:supernodeapp/common/components/page/submit_button.dart';
 import 'package:supernodeapp/common/components/page/title.dart';
 
 import 'state.dart';
@@ -14,17 +15,21 @@ Widget buildView(
     ConfirmState state, Dispatch dispatch, ViewService viewService) {
   var _ctx = viewService.context;
 
-  return pageFrame(context: viewService.context, children: [
-    pageNavBar(FlutterI18n.translate(_ctx, state.title),
-        onTap: () => Navigator.pop(viewService.context)),
-    title(FlutterI18n.translate(_ctx, state.title)),
-    done(
-        success:
-            state.content.contains('successful') || state.title == 'confirmed'),
-    pageContent(FlutterI18n.translate(_ctx, state.content)),
-    submitButton(
-      FlutterI18n.translate(_ctx, 'done'),
-      onPressed: () => Navigator.pop(viewService.context),
-    )
-  ]);
+  return pageFrame(context: viewService.context,
+      children: [
+        pageNavBar(FlutterI18n.translate(_ctx, state.title),
+            leadingWidget: SizedBox(),
+            onTap: () => Navigator.pop(viewService.context)),
+        title(FlutterI18n.translate(_ctx, 'confirmed')),
+        done(
+            success:
+            FlutterI18n.translate(_ctx, state.content).contains('successful') || state.title == 'confirmed'),
+        pageContent(FlutterI18n.translate(_ctx, state.content)),
+        xbigColumnSpacer(),
+        PrimaryButton(
+          buttonTitle: FlutterI18n.translate(_ctx, 'done'),
+          onTap: () => Navigator.pop(viewService.context),
+          minWidth: double.infinity
+        )
+      ]);
 }
