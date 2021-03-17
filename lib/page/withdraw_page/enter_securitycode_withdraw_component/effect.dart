dbcc5ef6a32dbc5c7e3640789bee6518f05e7e8d
miljan-mxc <72385784+miljan-mxc@users.noreply.github.com>
2021-03-17 12:44:13 +0100

    Removing old WithdrawPage based on fish redux

--- lib/page/withdraw_page/enter_securitycode_withdraw_component/effect.dart
+++
@@ -1,7 +0,0 @@
-import 'package:fish_redux/fish_redux.dart';
-
-import 'state.dart';
-
-Effect<EnterSecurityCodeWithdrawState> buildEffect() {
-  return combineEffects(<Object, Effect<EnterSecurityCodeWithdrawState>>{});
-}
