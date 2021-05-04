5a1d8e5629b0f8a1f46eaeeabdfe31492b4c9f22
miljan-mxc <72385784+miljan-mxc@users.noreply.github.com>
2021-05-04 18:11:51 +0200

    If previous session was demo mode, logout

--- lib/main.dart
+++ lib/main.dart
@@ -326,7 +326,7 @@ class MxcApp extends StatelessWidget {
                     );
                   },
                   onGenerateInitialRoutes: (state, s) => [
-                    context.read<SupernodeCubit>().state.session == null
+                    context.read<SupernodeCubit>().state.session == null || context.read<SupernodeCubit>().state.session.userId == -1 /* demoMode */
                         ? route((ctx) => LoginPage())
                         : route((ctx) => HomePage()),
                   ],
