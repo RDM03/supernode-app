7eb631e31c6643a1ac06bd6f462a05ff7ee8e5dd
miljan-mxc <72385784+miljan-mxc@users.noreply.github.com>
2021-05-21 14:33:30 +0200

    BarGraph x-axis labels

--- lib/common/components/widgets/bar_graph.dart.dart
+++ lib/common/components/widgets/bar_graph.dart.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:supernodeapp/theme/colors.dart';
+import 'package:supernodeapp/theme/font.dart';
 
 class BarGraph extends StatelessWidget {
   /// List of values [0-1]
   final List<double> graphValues;
+  /// List labels
+  final List<String> xAxisLabels;
   /// Number of graph bars visible. The rest of values are scrolled horizontally
   final int barsOnScreen;
   /// Color of BarGraph
@@ -20,7 +23,7 @@ class BarGraph extends StatelessWidget {
   double scrollableWidth;
   int currentBar = 0;
 
-  BarGraph(this.graphValues, this.barsOnScreen, this.widgetWidth, {this.widgetHeight= 200, this.graphColor = minerColor, this.notifyGraphBarScroll}) {
+  BarGraph(this.graphValues, this.barsOnScreen, this.widgetWidth, {this.xAxisLabels, this.widgetHeight= 200, this.graphColor = minerColor, this.notifyGraphBarScroll}) {
     spaceBetweenLines = (widgetWidth - (barsOnScreen * barWidth)) / (barsOnScreen - 1);
     scrollableWidth = barWidth * graphValues.length + spaceBetweenLines * (graphValues.length - 1);
     if (notifyGraphBarScroll != null) {
@@ -48,6 +51,7 @@ class BarGraph extends StatelessWidget {
           painter: GraphPainter(
               lineColor: graphColor,
               completePercents: graphValues,
+              labels: xAxisLabels,
               visibleWidth: widgetWidth,
               linesOnScreen: barsOnScreen,
               spaceBetweenLines: spaceBetweenLines,
@@ -66,11 +70,12 @@ class BarGraph extends StatelessWidget {
 class GraphPainter extends CustomPainter{
   final Color lineColor;
   final List<double> completePercents;
+  final List<String> labels;
   final double visibleWidth;
   final int linesOnScreen;
   final double spaceBetweenLines;
   final double lineWidth;
-  GraphPainter({this.lineColor, this.completePercents, this.visibleWidth, this.linesOnScreen, this.spaceBetweenLines, this.lineWidth});
+  GraphPainter({this.lineColor, this.completePercents, this.labels, this.visibleWidth, this.linesOnScreen, this.spaceBetweenLines, this.lineWidth});
   @override
   void paint(Canvas canvas, Size size) {
     Paint line = new Paint()
@@ -85,11 +90,36 @@ class GraphPainter extends CustomPainter{
       ..strokeWidth = lineWidth;
 
     double x;
+    double labelHeight = 0;
+    if (labels != null)
+      labelHeight = 20;
     for (int i = 0; i < completePercents.length; i++) {
       x = lineWidth / 2 + i * (lineWidth + spaceBetweenLines);
-      canvas.drawLine(new Offset(x, size.height - lineWidth / 2), new Offset(x,  lineWidth / 2), lineBkgrd);
-      canvas.drawLine(new Offset(x, size.height - lineWidth / 2),
-          new Offset(x, (size.height - lineWidth) * (1 - completePercents[completePercents.length - 1 - i]) + lineWidth / 2), line);
+      canvas.drawLine(new Offset(x, size.height - lineWidth / 2 - labelHeight), new Offset(x,  lineWidth / 2), lineBkgrd);
+      canvas.drawLine(new Offset(x, size.height - lineWidth / 2 - labelHeight),
+          new Offset(x, (size.height - lineWidth - labelHeight) * (1 - completePercents[completePercents.length - 1 - i]) + lineWidth / 2), line);
+      if (labels != null) {
+        final textSpan = TextSpan(
+          text: labels[labels.length - 1 - i],
+          style: kSmallFontOfBlack,
+        );
+        final textPainter = TextPainter(
+          text: textSpan,
+          maxLines: 1,
+          textDirection: TextDirection.ltr,
+        );
+        textPainter.layout(
+          minWidth: 0,
+          maxWidth: spaceBetweenLines,
+        );
+        double xTextStart = x - textPainter.width / 2;
+        if (labels.length - 1 - i == 0)
+          xTextStart = x - textPainter.width + lineWidth / 2;
+        if (i == 0)
+          xTextStart = x - lineWidth / 2;
+        final offset = Offset(xTextStart, size.height - textPainter.height);
+        textPainter.paint(canvas, offset);
+      }
     }
   }
   @override
