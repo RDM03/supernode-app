dc7dc8960046cd7fb808088481ba7f683c8a9ccc
Ilya Beregovskiy <festeloqq@gmail.com>
2021-06-09 04:51:34 +0300

    split main - APK & GP

--- lib/common/components/update_dialog.dart
+++ lib/common/components/update_dialog.dart
@@ -3,22 +3,30 @@ import 'dart:io';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_appcenter/flutter_appcenter.dart';
 import 'package:flutter_i18n/flutter_i18n.dart';
-import 'package:supernodeapp/configs/sys.dart';
 
-Future<bool> updateDialog(BuildContext ctx) {
-  return FlutterAppCenter.checkForUpdate(ctx,
-      channelGooglePlay: Sys.channelGooglePlay,
-      downloadUrlAndroid: Sys.downloadUrlAndroid,
-      dialog: {
-        'title': FlutterI18n.translate(ctx, 'update_dialog_title'),
-        'subTitle': FlutterI18n.translate(ctx, 'update_dialog_subTitle'),
-        'content': FlutterI18n.translate(ctx, 'update_dialog_content'),
-        'confirmButtonText': Platform.isAndroid
-            ? FlutterI18n.translate(ctx, 'update_dialog_confirm')
-            : 'App Store',
-        'middleButtonText': Platform.isAndroid ? '' : 'TestFlight',
-        'cancelButtonText': FlutterI18n.translate(ctx, 'update_dialog_cancel'),
-        'downloadingText':
-            FlutterI18n.translate(ctx, 'update_dialog_downloading')
-      });
+class Updater {
+  final UpdateHandler handler;
+
+  static Updater _instance;
+  static Updater get instance => _instance;
+
+  Updater._(this.handler);
+
+  static void init(UpdateHandler updateHandler) {
+    _instance = Updater._(updateHandler);
+  }
+
+  Future<bool> updateDialog(BuildContext ctx) {
+    return FlutterAppCenter.checkForUpdate(ctx, handler, dialog: {
+      'title': FlutterI18n.translate(ctx, 'update_dialog_title'),
+      'subTitle': FlutterI18n.translate(ctx, 'update_dialog_subTitle'),
+      'content': FlutterI18n.translate(ctx, 'update_dialog_content'),
+      'confirmButtonText': Platform.isAndroid
+          ? FlutterI18n.translate(ctx, 'update_dialog_confirm')
+          : 'App Store',
+      'middleButtonText': Platform.isAndroid ? '' : 'TestFlight',
+      'cancelButtonText': FlutterI18n.translate(ctx, 'update_dialog_cancel'),
+      'downloadingText': FlutterI18n.translate(ctx, 'update_dialog_downloading')
+    });
+  }
 }
